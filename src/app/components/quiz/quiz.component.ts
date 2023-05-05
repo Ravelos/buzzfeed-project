fb612302530e7f08e5dae797f068c4c1a94caa78
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-05 20:54:55 -0300

    Creates the methods playerChoice and nextStep

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 
 @Component({
   selector: 'app-quiz',
@@ -19,6 +20,29 @@ export class QuizComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false;
+      this.title = quiz_questions.title;
+
+      this.questions = quiz_questions.questions;
+      this.selectedQuestion = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(value:string){
+    this.answers.push(value);
+    console.log(this.answers)
+  }
+
+  async nextStep(){
+    if(this.questionMaxIndex > this.questionIndex){
+      this.selectedQuestion = this.questions[this.questionIndex];
+    }else{
+      this.finished = true;
+    }
   }
 
 }
